7acaed2264fbea2225c5d9a54d2489cc77f4773b
JangIBin <59593043+JangIBin@users.noreply.github.com>
2022-06-07 09:05:36 +0900

    2022-06-07 푸터 수정

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,7 +6,8 @@ function Footer() {
   return(
 		<div className="Footer">
 			<div className="footer-wrap">
-				CopyRight Jang IBin
+				본 페이지는 상업적 목적이 아닌 개인 포트폴리오용으로 제작되었습니다.<br /><br />
+				© 2021 Jang, I-Bin All Rights Reserved.
 			</div>
 		</div>
   )
